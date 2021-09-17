0cd7c00145fad28a49e1d0ac3dccaa8f1b791c31
jfr2102 <johannes@friedlein.eu>
2021-09-17 12:31:38 +0200

    write local timetsamp of fetcher thread to file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ import java.util.Collections;
 import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:9092");
         props.setProperty("group.id", "java-consumer");
@@ -23,24 +23,24 @@ public class Main {
         props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Collections.singleton("results"));
+
         while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(20));
             for (ConsumerRecord<String, String> record : records) {
-                // TODO: aus value timestamp auslesen und latency calculaten
-                // TODO: mit "" im korrekten jsonformat ausgeben in kafka emitter in storm topology
-                String value  = record.value();
+                long localTimestamp = System.currentTimeMillis();
+                String value = record.value();
                 JSONObject jsonValue = new JSONObject(value);
                 long window_end_timestamp = jsonValue.getLong("end_event_time");
                 long latency = record.timestamp() - window_end_timestamp;
 
-                String template = "%d; %s; %s; %d; %s; %d%n";
-                String csv_out = String.format(template, record.offset(), record.key(), record.value(), record.timestamp(), record.timestampType(), latency );
+                String template = "%d; %s; %s; %d; %s; %d; %d%n";
+                String csv_out = String.format(template, record.offset(), record.key(), record.value(),
+                        record.timestamp(), record.timestampType(), latency, localTimestamp);
                 try {
-                    Files.write(Paths.get("resultsoutput.csv"),csv_out.getBytes(), StandardOpenOption.APPEND);
+                    Files.write(Paths.get("resultsoutput.csv"), csv_out.getBytes(), StandardOpenOption.APPEND);
                 } catch (IOException e) {
-                    //exception handling left as an exercise for the reader
+                    // TODO
                 }
-                //System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
             }
         }
     }
